040b5adb97b30abbb53dc3d0a6c3bd5c7e5f1279
David C <dcorreap97@gmail.com>
2025-05-04 20:38:31 -0500

    refactor(backend)
    change sesion name

--- backend/routes/session.js
+++ backend/routes/session.js
@@ -3,7 +3,6 @@ import { login, register, logout } from '../controllers/sesion.js'
 
 const router = Router()
 
-// Rutas para el login y registro de usuarios
 router.post('/login', login)
 router.post('/register', register)
 router.post('/logout', logout)
