a3c126a02bac9ba919f3be7283eec71d39f215ed
David C <dcorreap97@gmail.com>
2025-05-29 15:37:26 -0500

    feat(frontend)
    change validation to form register

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,36 +1,61 @@
+/* eslint-disable no-undef */
 const btnRegister = document.getElementById('register-button')
 
 btnRegister.addEventListener('click', async (e) => {
-  Register()
+  const isValidate = await validateForm()
+
+  if (isValidate) {
+    register()
+  }
 })
 
-async function Register () {
-  const name = document.getElementById('floating-name')
-  const email = document.getElementById('floating-email')
-  const password = document.getElementById('floating-password')
-  const role = document.getElementById('floating-role')
-  const dni = document.getElementById('floating-document')
+async function validateForm () {
+  const form = document.getElementById('register-form')
 
-  const response = await fetch('/api/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: name.value,
-      email: email.value,
-      password: password.value,
-      role: role.value,
-      dni: dni.value
-    })
-  })
-
-  if (response.ok) {
-    const data = await response.json()
-    console.log('Registration successful:', data)
-    window.location.href = '/login'
+  if (form.checkValidity()) {
+    return true
   } else {
-    const errorData = await response.json()
-    console.error('Registration failed:', errorData)
+    form.classList.add('was-validated')
+    return false
+  }
+}
+
+async function register () {
+  try {
+    const name = document.getElementById('floating-name')
+    const email = document.getElementById('floating-email')
+    const password = document.getElementById('floating-password')
+    const role = document.getElementById('floating-role')
+    const dni = document.getElementById('floating-document')
+
+    const response = await fetch('/session/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        role: role.value,
+        dni: dni.value
+      })
+    })
+
+    if (response.ok) {
+      const data = await response.json()
+      Swal.fire('Registration successful', `Welcome ${data.name}`, 'success').then(() => {
+        window.location.href = '/login'
+      })
+    } else {
+      Swal.fire(
+        'Registration failed',
+        'Please check your input and try again.',
+        'error'
+      )
+    }
+  } catch (error) {
+    console.error('Error during registration:', error)
+    Swal.fire('Connection failed', error.message, 'error')
   }
 }
