eaadd9183781061497fc741c49b704844d386688
David C <dcorreap97@gmail.com>
2025-05-30 17:02:06 -0500

    feat(backend)
    add route to upload files

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,26 @@ import DatabaseConnection from './db.js'
 import userRoutes from '../backend/routes/user.js'
 import sessionRoutes from './routes/session.js'
 import pagesRoutes from './routes/pages.js'
+import uploadRoutes from './routes/uploadFiles.js'
 
 // start server
 const app = express()
-
-// set port
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/login')
-})
-
-// connect to database
-DatabaseConnection()
 
 // middlewares
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.static('frontend')) // incluir el frontend
 
+// connect to database
+DatabaseConnection()
+
 // routes
 app.use('/', pagesRoutes)
 app.use('/users', userRoutes)
 app.use('/session', sessionRoutes)
+app.use('/upload', uploadRoutes)
+
+// set port
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000/login')
+})
