6c06ac8bed89121247b839cd0158ed5b17a5724e
David C <dcorreap97@gmail.com>
2025-05-30 22:33:27 -0500

    feat(frontend)
    change css and img routes to relative

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 })
 
-async function updateProfile (profile) {
+async function updateProfile(profile) {
   const name = document.getElementById('name')
   const email = document.getElementById('email')
   try {
@@ -20,7 +20,7 @@ async function updateProfile (profile) {
   }
 }
 
-async function getLoggedUser () {
+async function getLoggedUser() {
   try {
     const response = await fetch('http://localhost:3000/session/loggedUser', {
       method: 'GET',
@@ -40,3 +40,32 @@ async function getLoggedUser () {
     console.error('Error fetching user:', error)
   }
 }
+
+const form = document.getElementById('uploadForm')
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const fileInput = document.getElementById('fileInput')
+  const formData = new FormData()
+  formData.append('file', fileInput.files[0]) // clave "file"
+
+  try {
+    const response = await fetch('http://localhost:3000/api/upload', {
+      method: 'POST',
+      body: formData
+    })
+
+    const result = await response.json()
+    console.log(result)
+
+    if (response.ok) {
+      alert('Archivo subido con éxito:\n' + result.url)
+    } else {
+      alert('Error al subir archivo.')
+    }
+  } catch (error) {
+    console.error('Error:', error)
+    alert('Fallo al subir el archivo.')
+  }
+})
